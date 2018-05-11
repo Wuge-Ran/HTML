e4f9c068b471ca332013937934b5bfad2dc2a985
Wuge-Ran <15208219011@163.com>
2018-05-11 16:33:17 +0800

    new one

--- works/05.09/js/message.js
+++ works/05.09/js/message.js
@@ -11,16 +11,16 @@ oImg.onclick=function (){
     }else{
         oImg.src='img/1.png';
         i=true;
-    }
+    } 
 }
 oSend.onclick=function (){
     if(oText.value==''){
         alert('输入点内容吧！');
-    }else if(i==true){
+    }else if(i){
         // 问题：oImg.src=='img/1.png',读取的是绝对路径
         oWrap.innerHTML='<div class="line">' + '<img src="img/1.png" alt="" id="img1">' + '<div class="right">' + oText.value + '</div>' + '</div>' + oWrap.innerHTML;
         oText.value='';
-    }else if(i==false){
+    }else{
         // 问题：oImg.src=='img/1.png',读取的是绝对路径
         oWrap.innerHTML='<div class="line">' + '<img src="img/2.png" alt="" id="img2">' + '<div class="left">' + oText.value + '</div>' + '</div>' + oWrap.innerHTML;
         oText.value='';
